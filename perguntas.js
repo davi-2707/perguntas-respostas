93b6f79810dab237a7f19e0d6ab26c8d4a487022
davi-2707 <168192367+davi-2707@users.noreply.github.com>
2024-11-28 18:11:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quantos continentes tem no planeta Terra?',
+    'Existem 5 continentes no planeta Terra.'
+);
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quantos dias tem um ano?',
+    '365 dias.'
+);
+criaCartao(
+    'Geografia',
+    'A Austrália faz parte de qual continente?',
+    'Oceania.'
+);
+criaCartao(
+    'História',
+    'Quantos anos durou a Primeira Guerra Mundial?',
+    'Durou aproximadamente 4 anos.'
+);
+criaCartao(
+    'Química',
+    'A quantos graus a água evapora?',
+    'A 100 graus celsius.'
+);
+criaCartao(
+    'Português',
+    'Qual é o infinitivo do verbo "pulou"?',
+    'Pular.'
+);
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é o país que tem mais habitantes no Planeta?',
+    'A China.'
+);
